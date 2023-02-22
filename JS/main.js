d378efaf90ac80e26a1941553131b9bcb332b52c
A-D-E <a.user.de@gmail.com>
2023-02-22 18:20:58 +0100

    js

---
+++ JS/main.js
@@ -0,0 +1,16 @@
+import "./style.css"
+const input = document.querySelectorAll('input')
+const summe = document.querySelector('#summe')
+const btn = document.querySelector('.button')
+btn.addEventListener('click', function(e){
+  const green = "green"
+  
+  summe.style.backgroundColor === "green"
+  ? summe.style.backgroundColor = "red"
+  : summe.style.backgroundColor = "green"
+})
+
+
+
+
+
