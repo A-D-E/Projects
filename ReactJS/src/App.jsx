443c0527a0e82343aa48476991c3867ce9e243ee
Andrej <a.user.de@gmail.com>
2023-03-31 18:08:41 +0200

    add react

---
+++ ReactJS/src/App.jsx
@@ -0,0 +1,17 @@
+import './App.scss'
+import Header from './layout/Header'
+import Content from './layout/Content'
+import Footer from './layout/Footer'
+
+function App() {
+
+  return (
+    <div className="App">
+      <Header />
+      <Content />
+      <Footer />
+    </div>
+  )
+}
+
+export default App
